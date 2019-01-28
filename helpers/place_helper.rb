ace15140614d06b42e9ef657f775148a6efe3db4
Brett Jones <hello@brettjones.me>
2019-01-28 15:27:50 +0800

    Additional work Refactored activity section and added transport infrastructure

--- helpers/place_helper.rb
+++ helpers/place_helper.rb
@@ -2,4 +2,12 @@ module PlaceHelper
   def find_place(query)
     data.places.find { | slug, place | slug == query}
   end
+
+  def find_meal(query)
+    data.meals.find { | slug, meal | slug == query}
+  end
+
+  def find_transport(query)
+    data.transport.find { | slug, transport | slug == query}
+  end
 end
